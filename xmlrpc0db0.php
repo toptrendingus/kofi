a91a12cbec4d09ea30fca65f619df42b9287aa2c
missnorwyu32 <144135159+missnorwyu32@users.noreply.github.com>
2024-02-09 20:11:44 +0600

    up

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://you.csusb.edu</homePageLink>
+		<homePageLink>https://you.csustan.edu</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://you.csusb.edu/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://you.csusb.edu/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://you.csusb.edu/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://you.csusb.edu/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://you.csusb.edu/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://you.csustan.edu/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://you.csustan.edu/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://you.csustan.edu/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://you.csustan.edu/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://you.csustan.edu/wp-json/" />
 			</apis>
 	</service>
 </rsd>
